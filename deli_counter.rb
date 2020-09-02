95a72221e889af698a6011f0f8198ddb6dd842a1
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-02 19:27:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    array.each_with_index do |name, index|
+      current_line << " #{index +1}. #{name}"
+    end
+    puts current_line
+  end
+end
+
